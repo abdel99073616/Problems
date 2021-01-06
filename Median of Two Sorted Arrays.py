ca782817e023dcb299006175e3f714277ab55fd1
Abdo Mohamed <abdel99073616@gmail.com>
2021-01-06 05:07:43 +0200

    2

--- Median of Two Sorted Arrays.py
+++ Median of Two Sorted Arrays.py
@@ -1,1 +1,15 @@
-num1 = input("")
+num1 = [1,3]
+num2 = [2 , 4]
+
+num3 = num1 + num2
+num3 = sorted(num3)
+
+sumlen = len(num1) + len(num2)
+
+if sumlen%2 == 0:
+    med = int((sumlen)/2)
+    med2 = int(((sumlen)/2) -1)
+    print((med+med2)/2)
+else:
+    l = int((sumlen)/2)
+    print(num3[l])
